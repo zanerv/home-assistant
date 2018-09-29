61b28e3eaf07015c9b491c67d96a4f7a1e5eff8f
Daniel Iversen <Daniel.Iversen@tibber.com>
2018-09-29 09:46:13 +0200

    met no 0.3.0

--- homeassistant/components/weather/met.py
+++ homeassistant/components/weather/met.py
@@ -18,7 +18,7 @@ from homeassistant.helpers.event import (async_track_utc_time_change,
                                          async_call_later)
 import homeassistant.util.dt as dt_util
 
-REQUIREMENTS = ['pyMetno==0.2.0']
+REQUIREMENTS = ['pyMetno==0.3.0']
 
 _LOGGER = logging.getLogger(__name__)
 
